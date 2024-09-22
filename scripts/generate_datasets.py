32bc87a79b51562219c0b4488fdaeaa7e46bb332
Aditya Patange <contact.adityapatange@gmail.com>
2024-09-22 08:12:52 +0530

    scripts/generate_datasets: add case by case generation and write only to csv

--- scripts/generate_datasets.py
+++ scripts/generate_datasets.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 import traceback
 import csv
+import random
 
 load_dotenv()
 
@@ -16,6 +17,9 @@ import json
 MAX_ROWS = 10
 DATASET_FILE_NAME = "promptena_core_dataset"
 
+llm_options = LLMOptions()
+llm = LLM(llm_options)
+
 
 class PromptenaCoreDatasetRowItem(BaseModel):
     prompt: str
@@ -24,16 +28,22 @@ class PromptenaCoreDatasetRowItem(BaseModel):
     confidence: float
 
 
-def generate_dataset():
-    llm_options = LLMOptions()
-    llm = LLM(llm_options)
+def generate_case(is_context_sufficient: bool) -> PromptenaCoreDatasetRowItem:
 
-    prompt = """
-                Generate a prompt for a random task. The prompt should be a question that requires a short answer.
-                The prompt should be related to a task that a user would like to get help with.
-                The purpose of the prompt is that we want to train an ML model to determine if a prompt has enough context or not.
-                This is so that we can decide whether more context needs to be fetched from data storage systems or not.  
-                The generated prompt length can be between 1 sentence to 10 sentences. 
+    context_prompt = "There is sufficient context so set 'is_context_sufficient' to True and provide a prompt with context. This prompt doesn't require fetching of additional data."
+
+    if not is_context_sufficient:
+        context_prompt = "There is insufficient context so set 'is_context_sufficient' to False and provide a prompt without context. This prompt requires fetching of additional data."
+
+    num_sentences = random.randint(1, 10)
+
+    prompt = f"""
+                I'm building a RAG system. 
+                For that I need to train a Deep Neural Network to classify whether a prompt has enough 'context' or not. Context is required when the response to the prompt requires information that the LLM does not know or understand. 
+                The prompt is between 1 to 10 sentences (THIS IS VERY IMPORTANT: VARY THE PROMPT SIZE BETWEEN 1 TO 10 SENTENCES). I have given you the schema that I want your answer to be in. Give me 20 prompts and their 'is_context_sufficient' status. 
+                Vary the topics between different business domains and problem statements. Be as creative as you can. You should mimic how an actual user would prompt an LLM.
+
+                {context_prompt}
              """
 
     system_prompt = "You are a Synthetic Data Generation AI Agent. Generate high-quality and relevant synthetic data for the given task."
@@ -45,11 +55,22 @@ def generate_dataset():
         verbose=True,
     )
 
+    return llm.generate_object(options=generate_options)
+
+
+def generate_dataset():
+    positive_cases = MAX_ROWS // 2
+    negative_cases = MAX_ROWS - positive_cases
+
     dataset = []
 
-    for _ in range(MAX_ROWS):
-        response = llm.generate_object(options=generate_options)
-        dataset.append(response.model_dump())
+    for i in range(positive_cases):
+        obj = generate_case(is_context_sufficient=True)
+        dataset.append(obj.model_dump())
+
+    for i in range(negative_cases):
+        obj = generate_case(is_context_sufficient=False)
+        dataset.append(obj.model_dump())
 
     return dataset
 
@@ -121,7 +142,7 @@ def run():
             "generate_datasets: writing dataset to file. ",
             {"rows_generated": len(dataset)},
         )
-        write_dataset_to_file(dataset=dataset, format="json")
+        # write_dataset_to_file(dataset=dataset, format="json")
         write_dataset_to_file(dataset=dataset, format="csv")
     except Exception as e:
         print("generate_datasets: script failed. ")
